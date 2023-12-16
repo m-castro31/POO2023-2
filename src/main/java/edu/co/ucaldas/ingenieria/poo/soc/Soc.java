0ed387d0c416921fab086fd0a500b61f36dcf12f
m-castro31 <castro.dmariana@gmail.com>
2023-12-16 16:59:31 -0500

    commit

---
+++ src/main/java/edu/co/ucaldas/ingenieria/poo/soc/Soc.java
@@ -0,0 +1,10 @@
+package edu.co.ucaldas.ingenieria.poo.soc;
+
+import edu.co.ucaldas.ingenieria.poo.soc.ui.LoginUI;
+
+public class Soc {
+
+    public static void main(String[] args) {
+                new LoginUI().setVisible(true);
+    }
+}
